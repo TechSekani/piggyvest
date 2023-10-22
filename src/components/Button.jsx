ceb8b775e39fe027d6f01e3c8e54960e79b3df03
Sekani <116392903+TechSekani@users.noreply.github.com>
2023-10-22 16:08:01 +0100

    Update Button.jsx

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,7 @@ import { Icon } from '@iconify/react';
 
 const Button = ({text, style, }) => {
   return (
-    <button className={` flex items-center justify-center gap-1 font-[Eina01Bold] border border-[#0C1825] text-[#0C1825] rounded-lg py-3 md:py-3 px-3 md:px-5 ${style}`}>
+    <button className={` flex items-center justify-center gap-1 font-[Eina01Semibold] border border-[#0C1825] text-[#0C1825] rounded-lg py-3 md:py-3 px-3 md:px-5 ${style}`}>
         {text == 'Get on iPhone' && <Icon icon="mdi:apple" width={20} />}
         {text == 'Get on Android' && <Icon icon="ion:logo-google-playstore" width={20} />}
         {text}
